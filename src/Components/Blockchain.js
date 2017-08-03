673e1ff997483d56dad7220977304efbef8129b5
Viktor Seč <hey@viktorsec.com>
2017-08-03 13:04:45 +0200

    Fetch rates from Blockchain mock api

--- src/Components/Blockchain.js
+++ src/Components/Blockchain.js
@@ -7,15 +7,17 @@ class Blockchain extends Component {
     const address = "1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj"
     this.state = {
       address: address,
-      balance: 0,
     }
     this.getAddressBalance = this.getAddressBalance.bind(this)
+    this.getUSDRate = this.getUSDRate.bind(this)
   }
 
   componentDidMount() {
     const {address} = this.state
     this.getAddressBalance(address)
       .then(res => this.setState({balance: res}))
+    this.getUSDRate()
+      .then(res => this.setState({usdRate: res}))
   }
 
   getAddressBalance(address) {
@@ -29,13 +31,26 @@ class Blockchain extends Component {
         const bitcoinBalance = res / 1000
         return bitcoinBalance
       })
-      .catch((e) => console.log(e))
+      .catch((e) => console.log('API error', e))
+  }
+
+  getUSDRate() {
+    const hub = 'https://private-anon-0bf4dd50e5-blockchaininfo.apiary-mock.com/q/'
+    const query = `${hub}24hrprice`
+
+    return fetch(query)
+      .then((res) => res.json())
+      .catch((e) => console.log('API error', e))
   }
 
   render() {
-    const {address, balance} = this.state
+    const {address, balance, usdRate} = this.state
+    const balanceUSD = balance * usdRate
     return (
-      <div>Current balance for {address}: {balance}</div>
+      <div>
+        <p>Current balance for {address}: {balance}</p>
+        <p>That is about ${balanceUSD}.</p>
+      </div>
     )
   }
 
